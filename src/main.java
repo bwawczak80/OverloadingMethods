158f3ebdfd9a800017df10e946526b288c8ebcee
Brian Wawczak <bwawczak@students.mchenry.edu>
2018-09-12 01:15:53 -0500

    updated program
    overloadingMethods re-write

--- src/main.java
+++ src/main.java
@@ -1,44 +1,56 @@
 import java.util.Scanner;
 
-
 public class main {
 
-
-    public static void main(String[] args){
-
+    public static void main (String[] args){
         Scanner scan = new Scanner(System.in);
-
         Scanner userInput = new Scanner(System.in);
-
         int userChoice;
+        userMenu();
+        userChoice = userInput.nextInt();
+        switch (userChoice) {
+            case 1:
+                System.out.println("What is your name?");
+                String custName1 = scan.nextLine();
+                System.out.println("What is your phone number?");
+                String custPhone1 = scan.nextLine();
+                customerInfo(custName1, custPhone1);
 
-        do {
-            System.out.println("Please enter your selection");
-            System.out.println("1.  Are you a Business customer");
-            System.out.println("2.  Are you a Residential customer");
-
-            userChoice = userInput.nextInt();
-        } while (userChoice != 1 && userChoice != 2);
-
-            if (userChoice == 1) {
-                customerInfo();
-
-
-            } else if (userChoice == 2) {
-                customerInfo();
-
-            } else {
-                System.out.println("That is not a valid option");
-
-
-            }
-
-        public static String customerInfo(){
-
+                break;
+            case 2:
+                System.out.println("What is your name?");
+                String custBus1 = scan.nextLine();
+                System.out.println("What is your business name?");
+                String custName2 = scan.nextLine();
+                System.out.println("What is your phone number?");
+                String custPhone2 = scan.nextLine();
+                customerInfo(custBus1, custName2, custPhone2);
+                break;
+            default:
+                System.out.println("That is not a valid choice.");
+                userMenu();
         }
-        }
+    }
+    private static void userMenu(){
+        System.out.println("What type of customer are you?\n ");
+        System.out.println("1.  Residential");
+        System.out.println("2.  Business");
 
+    }
+    private static void customerInfo(String s1, String s2){
+        System.out.println(s1);
+        System.out.println(s2);
+
+
+    }
+    private static void customerInfo(String s1, String s2, String s3){
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
 }
+
+
 
 
 
